b0c72dbdf8e534b66c717153dc98c28298f3cb62
bahrom04 <magdiyevbahrom@gmail.com>
2025-07-03 13:02:38 +0500

    chore: fix domirando properties assigning error

--- modules/nixos/users/domirando/default.nix
+++ modules/nixos/users/domirando/default.nix
@@ -10,7 +10,7 @@
   hashedPassword = lib.strings.concatStrings [
     "$y$j9T$7uoELFRD8t70X8IiTMHn/.$QQt5GT21792Vr5BpL53rY1fFQw2iOJ95MYlZRvvDU74"
   ];
-  description: "Maftuna Vohidjonovna";
+  description = "Maftuna Vohidjonovna";
   githubKeysUrl = "https://github.com/domirando.keys";
   sha256 = "0pd2bv95w9yv7vb3vn5qa1s3w1yc7b68qd5xbm8c6y7hmnhckygl";
   homePath = builtins.toPath ./. + "/home.nix";
