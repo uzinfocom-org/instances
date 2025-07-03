93cf8d81462440509dab081254c2046b722509ad
bahrom04 <magdiyevbahrom@gmail.com>
2025-07-03 10:46:20 +0500

    chore: update mkUsers arguments names

--- modules/nixos/users/bahrom04/default.nix
+++ modules/nixos/users/bahrom04/default.nix
@@ -17,10 +17,10 @@
   username = "bahrom04";
   hashedPassword = "$y$j9T$PEPMAZlXHzgRwOlum.4JA0$sh0uNM1PxrWmSjRdjYkw2iiDdquWF3z9CDAaasEwQ88";
   description = "Baxrom Raxmatov";
-  github_keys_url = "https://github.com/bahrom04.keys";
+  githubKeysUrl = "https://github.com/bahrom04.keys";
   sha256 = "0yazvxwvngyqapa7fz1qa7916c4w7km72smyl1im14mqbv8733k4";
-  home_nix_path = builtins.toPath ./. + "/home.nix";
-  mkUser = import ../mkUser.nix {
+  homePath = builtins.toPath ./. + "/home.nix";
+  mkUsers = lib.users.mkUsers {
     inherit
       inputs
       outputs
@@ -31,10 +31,10 @@
       username
       hashedPassword
       description
-      github_keys_url
+      githubKeysUrl
       sha256
-      home_nix_path;
+      homePath;
   };
 in {
-  config = mkUser;
+  config = mkUsers;
 }
