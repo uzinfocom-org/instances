f983d09db3ea13d7b95e47716570848954ae6008
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-07-28 19:39:13 +0500

    chore: upgrading mc version

--- nixos/laboratory/services/minecraft/default.nix
+++ nixos/laboratory/services/minecraft/default.nix
@@ -5,7 +5,7 @@
   ...
 }: let
   # Global variables
-  version = "1.21.5";
+  version = "1.21.8";
 in {
   imports = [inputs.minecraft.nixosModules.minecraft-servers];
 
@@ -26,7 +26,7 @@ in {
           server-port = 25565;
           difficulty = 3;
           gamemode = 0;
-          max-players = 50;
+          max-players = 10;
           motd = "\\u00A7f\\u00A7lWelcome to Uzinfocom Open Source's Server\\u00A7r\\n\\u00A7lMore at\:\\u00A7r \\u00A7nhttp\://laboratory.local";
           white-list = true;
           enable-rcon = true;
@@ -36,7 +36,6 @@ in {
 
         whitelist = {
           Orzklv = "3e220001-9544-48bb-8fd0-ca7139727207";
-          OwOssi = "14f4fba3-bd65-48d3-b212-2e77383c1b1d";
           Shakhz = "1c786ec6-0f65-4448-9ecf-ab53b75bf867";
           Nixxer = "8fdd80ef-c294-432d-b381-e43353a3db27";
           Bahrom04 = "15f652bf-33f9-4a08-aaf1-acc6a019c504";
@@ -47,11 +46,11 @@ in {
         symlinks = with pkgs; {
           "plugins/BlueMap" = ./plugins/BlueMap;
           "plugins/bluemap-spigot.jar" = fetchurl {
-            url = "https://cdn.modrinth.com/data/swbUV1cr/versions/wBnzat7x/bluemap-5.7-spigot.jar";
+            url = "https://cdn.modrinth.com/data/swbUV1cr/versions/ucikJabQ/bluemap-5.10-spigot.jar";
             hash = "sha256-mZ00IyPVe4GD1C6+B47MA9X/P+MQZ5dpaOX/hEec0d0=";
           };
           "plugins/Chunky-Bukkit.jar" = fetchurl {
-            url = "https://cdn.modrinth.com/data/fALzjamp/versions/mhLtMoLk/Chunky-Fabric-1.4.36.jar  ";
+            url = "https://cdn.modrinth.com/data/fALzjamp/versions/P3y2MXnd/Chunky-Bukkit-1.4.40.jar";
             hash = "sha256-vLttrvBeviawvhMk2ZcjN5KecT4Qy+os4FEqMPYB77U=";
           };
           "server-icon.png" = ./server-icon.png;
