e3f1d6dfadaf7bade50f8191aa95f0d578feb2f5
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-07-28 19:50:48 +0500

    fix: use less memory for server and allocate only two threads

--- nixos/laboratory/services/minecraft/default.nix
+++ nixos/laboratory/services/minecraft/default.nix
@@ -20,7 +20,7 @@ in {
         enable = true;
         openFirewall = true;
         package = pkgs.paperServers.${server};
-        jvmOpts = "-Xms24576M -Xmx24576M -XX:+UseG1GC -XX:ParallelGCThreads=4 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10";
+        jvmOpts = "-Xms8196M -Xmx8196M -XX:+UseG1GC -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10";
 
         serverProperties = {
           server-port = 25565;
