e9243ed9eb4fe86278ae7b4856003d0ce441b1d9
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-07-28 14:15:14 +0500

    chore: adding mc server web ui

--- nixos/laboratory/services/www.nix
+++ nixos/laboratory/services/www.nix
@@ -10,5 +10,17 @@
       cert = false;
       domain = "laboratory.local";
     };
+
+    hosts = {
+      "minecraft.local" = {
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8100";
+          proxyWebsockets = true;
+          extraConfig =
+            "proxy_ssl_server_name on;"
+            + "proxy_pass_header Authorization;";
+        };
+      };
+    };
   };
 }
