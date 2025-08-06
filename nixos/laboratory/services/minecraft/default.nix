8c522840031466fdaae83a0cbdefd89f652a0009
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-08-06 18:08:20 +0500

    chore: move mc server to /srv

--- nixos/laboratory/services/minecraft/default.nix
+++ nixos/laboratory/services/minecraft/default.nix
@@ -12,7 +12,6 @@ in {
   services.minecraft-servers = {
     enable = true;
     eula = true;
-    dataDir = "/var/lib/minecraft";
     managementSystem.systemd-socket.enable = true;
     servers = {
       oss = let
