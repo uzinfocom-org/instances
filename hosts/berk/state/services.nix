62e223b45b46c69546d88f5b674bca9b93660c43
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-12-22 22:12:59 +0500

    chore: adopting floss style matrix

---
+++ hosts/berk/state/services.nix
@@ -0,0 +1,101 @@
+{
+  config,
+  outputs,
+  ...
+}: {
+  imports = [
+    # Top level abstractions
+    outputs.nixosModules.web
+    outputs.nixosModules.mail
+    outputs.nixosModules.auth
+    # outputs.nixosModules.matrix
+
+    # Per app preconfigured abstractions
+    # ...
+  ];
+
+  sops.secrets = {
+    # Keycloak oriented services
+    "auth/database" = {
+      sopsFile = ../../../secrets/berk/auth.yaml;
+    };
+
+    # Mail oriented services
+    "mail/hashed" = {
+      key = "mail/hashed";
+      sopsFile = ../../../secrets/berk/mail.yaml;
+    };
+
+    # Matrix oriented secrets
+    # "matrix/server" = {
+    #   format = "binary";
+    #   owner = "matrix-synapse";
+    #   sopsFile = ../../../secrets/berk/matrix/server.hell;
+    # };
+    # "matrix/authentication" = {
+    #   format = "binary";
+    #   owner = "matrix-authentication-service";
+    #   sopsFile = ../../../secrets/berk/matrix/authentication.hell;
+    # };
+    # "matrix/push" = {
+    #   format = "binary";
+    #   owner = "matrix-sygnal";
+    #   path = "/var/lib/matrix-sygnal/sygnal.yaml";
+    #   sopsFile = ../../../secrets/berk/matrix/push.hell;
+    # };
+    # "matrix/ident" = {
+    #   format = "binary";
+    #   owner = "matrix-sygnal";
+    #   path = "/var/lib/matrix-sygnal/service_account.json";
+    #   sopsFile = ../../../secrets/berk/matrix/saccount.hell;
+    # };
+  };
+
+  # https://ns1.berk.uz
+  uzinfocom = {
+    www = {
+      enable = true;
+      domain = "ns1.uzberk.uz";
+    };
+
+    # https://(chat|matrix).berk.uz
+    # matrix = {
+    #   enable = true;
+    #   domain = "uzberk.uz";
+    #   cap = true;
+
+    #   synapse.extra-config-files = [
+    #     config.sops.secrets."matrix/server".path
+    #   ];
+
+    #   matrix-authentication-service.extra-config-files = [
+    #     config.sops.secrets."matrix/authentication".path
+    #   ];
+
+    #   matrix-sygnal = {
+    #     enable = true;
+    #     config-file = config.sops.secrets."matrix/push".path;
+    #   };
+    # };
+
+    # https://auth.uzberk.uz
+    auth = rec {
+      enable = true;
+      realm = "uzberk.uz";
+      password = config.sops.secrets."auth/database".path;
+    };
+
+    # (smtp|imap)://mail.uzberk.uz
+    mail = {
+      enable = true;
+      domain = "uzberk.uz";
+      password = config.sops.secrets."mail/hashed".path;
+    };
+
+    # *://*
+    # apps = {
+    #   # https://berk.uz
+    #   uchar.website.enable = true;
+    # };
+  };
+}
---
+++ modules/auth/default.nix
@@ -0,0 +1,80 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.uzinfocom.auth;
+in {
+  options = {
+    uzinfocom.auth = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Setup Keycloak IDM service.";
+      };
+
+      realm = lib.mkOption {
+        description = "Name of the realm, typically it's base domain";
+        type = lib.types.str;
+        default = "uchar.uz";
+      };
+
+      domain = lib.mkOption {
+        description = "The domain name where the service is hosted at";
+        type = lib.types.str;
+        default = "auth.${config.uzinfocom.auth.realm}";
+      };
+
+      password = lib.mkOption {
+        description = "Database password file path";
+        type = lib.types.path;
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.nginx.virtualHosts.${config.uzinfocom.auth.domain} = {
+      enableACME = true;
+      forceSSL = true;
+
+      extraConfig = ''
+        access_log /var/log/nginx/${config.uzinfocom.auth.domain}-access.log;
+        error_log /var/log/nginx/${config.uzinfocom.auth.domain}-error.log;
+      '';
+
+      locations = {
+        "= /" = {
+          extraConfig = ''
+            return 302 /realms/${cfg.realm}/account;
+          '';
+        };
+
+        "/" = {
+          extraConfig = ''
+            proxy_pass http://${
+              config.services.keycloak.settings.http-host
+            }:${
+              toString config.services.keycloak.settings.http-port
+            };
+            proxy_buffer_size 8k;
+          '';
+        };
+      };
+    };
+
+    services.keycloak = {
+      inherit (cfg) enable;
+      database = {
+        type = "postgresql";
+        passwordFile = cfg.password;
+      };
+      settings = {
+        hostname = config.uzinfocom.auth.domain;
+        http-host = "127.0.0.1";
+        http-port = 8080;
+        proxy-headers = "xforwarded";
+        http-enabled = true;
+      };
+    };
+  };
+}
