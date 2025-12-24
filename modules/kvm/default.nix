b48707d89ca210fe9dca6ef7ce34eff132163bd0
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-12-24 15:38:12 +0500

    chore: disable network card offloading

--- modules/kvm/default.nix
+++ modules/kvm/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   modulesPath,
   ...
@@ -27,5 +28,19 @@ in {
 
     # Qemu guest service
     services.qemuGuest.enable = lib.mkDefault true;
+
+    # Remove network card offloading
+    systemd.services.ethtool-ens18 = {
+      description = "ethtool-ens18";
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = "${pkgs.ethtool}/bin/ethtool -K ens18 gso off gro off tso off tx off rx off rxvlan off txvlan off";
+      };
+
+      # https://systemd.io/NETWORK_ONLINE/
+      wantedBy = ["network-pre.target"];
+    };
   };
 }
