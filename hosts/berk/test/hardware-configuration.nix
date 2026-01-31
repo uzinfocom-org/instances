8453ab8f0357470767d48c9cb110b1771a816fac
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-01-31 09:31:49 +0500

    fix: correct disk partition

---
+++ hosts/berk/test/hardware-configuration.nix
@@ -0,0 +1,48 @@
+{
+  inputs,
+  outputs,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    # Disko partitioning
+    inputs.disko.nixosModules.disko
+    ./disk-configuration.nix
+
+    # Not available hardware modules
+    (modulesPath + "/installer/scan/not-detected.nix")
+
+    # Virtualization environment
+    outputs.nixosModules.kvm
+  ];
+
+  # Bootloader shits
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "ata_piix"
+        "uhci_hcd"
+        "virtio_pci"
+        "virtio_scsi"
+        "sd_mod"
+        "sr_mod"
+      ];
+    };
+  };
+
+  uzinfocom = {
+    boot.uefi = true;
+    kvm.enable = true;
+    network = {
+      interface = "eno1";
+      ipv4 = {
+        subnet = 28;
+        address = "45.150.26.120";
+        gateway = "45.150.26.117";
+      };
+    };
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
---
+++ hosts/berk/test/services.nix
@@ -0,0 +1,62 @@
+{
+  config,
+  outputs,
+  ...
+}: {
+  imports = [
+    # Top level abstractions
+    outputs.nixosModules.web
+    outputs.nixosModules.mail
+    outputs.nixosModules.auth
+    outputs.nixosModules.matrix
+
+    # Per app preconfigured abstractions
+    outputs.nixosModules.apps.uchar-website
+  ];
+
+  sops.secrets = {
+    # Matrix oriented secrets
+    "matrix/server" = {
+      format = "binary";
+      owner = "matrix-synapse";
+      sopsFile = ../../../secrets/berk/matrix/server-x.hell;
+    };
+    "matrix/authentication" = {
+      format = "binary";
+      owner = "matrix-authentication-service";
+      sopsFile = ../../../secrets/berk/matrix/authentication-x.hell;
+    };
+  };
+
+  uzinfocom = {
+    # https://nsx.uzberk.uz
+    www = {
+      enable = true;
+      domain = "nsx.uzberk.uz";
+    };
+
+    # https://(chat|matrix).mg.uzberk.uz
+    matrix = {
+      enable = true;
+      domain = "plazma.uzberk.uz";
+      cap = true;
+
+      synapse.extra-config-files = [
+        config.sops.secrets."matrix/server".path
+      ];
+
+      matrix-authentication-service.extra-config-files = [
+        config.sops.secrets."matrix/authentication".path
+      ];
+    };
+
+    # *://*
+    apps = {
+      # https://plazma.berk.uz
+      uchar.website = {
+        enable = true;
+        domain = "plazma.uzberk.uz";
+      };
+    };
+  };
+}
