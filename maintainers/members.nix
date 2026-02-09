28c4907040139f63ded796e49f710e8fd269324b
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-02-09 14:16:07 +0000

    chore: add sud0pacman to members

--- maintainers/members.nix
+++ maintainers/members.nix
@@ -47,4 +47,12 @@
     password = "$y$j9T$mi9V.JCa4o3.QO2X28vJo1$KzXKjk/5sN4lbsvF8BIEIwhKCL34e0gJxZEe.MMoT1B";
     keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGU7pRKuTWfzRy8+Hb6vKz4+FNKfDzKA0HCLw+cxDCVXqsCBJPZXTfUZV1fxfBhfgn2IBOw99DbnmRaYeSm48ZB7V0xwqgM8Ucy2m4MJytvPbyjoEcfV434J3Xm+1R5P4tn5BvFPPseBBFrahsKXvakT07hiEJe6S28KuC3zvMN/cORfGuViGuZRslRuT3ozd8pJtDcWSod5f3ek59qwYrC8KS8ljR7kBJWgdJvAOyifuDd9POh4TcbXOykcDqYKlZlWLnFoZcCE3QUcOAELyBffEtMFRd/4N+Mgwdf6Y4YjspHNDfnSKRgNQVH/zYBnIV9jt/umdAyN9Kby0v/EGv9HI0Kb5t2/eCLPCDSyb4AQChb25xMTkGXcXcqIrLCWl6oR1/QfqUfuC8KJRp5Nj9saoi9pxtzAqU4/EXXL1EwYHaICK4LOYW+2la05Pv8wzX4ne9Xpoo0jJNCHioYacvJC1noWrDSmRU6oEhQqHKGBQU0drC/pYLmZhjAhi0JQE="];
   };
+
+  sud0pacman = {
+    username = "sud0pacman";
+    description = "Muhammad Ganiyev";
+    password = "$6$password$hzeoZtLSZiDD70sHayspuRmk1FRoLSOaBDi5TGwKHeb1EW1wvIIQliOv1SY4KmKwfr37dmYKC3q8I5L0jcfZG1";
+    keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIObsRsvvIZp3fIxmVTV9Xj4DmtyRIi+yJohKGUNBNwUu mukhammad.kammoliddin@gmail.com"];
+  };
+
 }
