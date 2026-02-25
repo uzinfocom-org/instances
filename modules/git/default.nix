912c805f779104c3073741f6821e888b3ff7e2d5
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-02-25 06:46:30 +0500

    chore: remove keycloak force for now

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -87,12 +87,12 @@ in
             '';
           };
 
-          "/user/login" = {
-            priority = 100;
-            extraConfig = ''
-              return 302 /user/oauth2/keycloak;
-            '';
-          };
+          # "/user/login" = {
+          #   priority = 100;
+          #   extraConfig = ''
+          #     return 302 /user/oauth2/keycloak;
+          #   '';
+          # };
 
           "/" = {
             priority = 200;
