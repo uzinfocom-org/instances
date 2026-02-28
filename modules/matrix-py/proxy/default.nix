46df8fb285d389b84f6284e7b42a70f98b6e3508
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-02-28 06:04:54 +0500

    chore: add path for mautrix-telegram

--- modules/matrix-py/proxy/default.nix
+++ modules/matrix-py/proxy/default.nix
@@ -204,6 +204,13 @@ in
             locations = lib.foldl' lib.recursiveUpdate { } (
               [
                 {
+                  # For telegram
+                  "/c3c3f34b-29fb-5feb-86e5-98c75ec8214b" = {
+                    priority = 100;
+                    proxyPass = "http://127.0.0.1:8009";
+                    extraConfig = commonHeaders;
+                  };
+
                   # Forward to the auth service
                   "~ ^/_matrix/client/(.*)/(login|logout|refresh)" = {
                     priority = 100;
